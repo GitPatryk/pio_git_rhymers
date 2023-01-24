bde3c1dea85f6aad25c9af57d1887a61dc9ae118
Patryk Balcerowski <236379@edu.p.lodz.pl>
2023-01-24 20:39:26 +0100

    2.Refaktoryzacja kodu i odpowiedz na pytania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+/* HanoiRhymer wymagał formatowania w linii piątej oraz 11-15 *
+* Klawisze alt + <- i alt + ->  przełączają nas pomiędzy plikami otwartymi w programie, po kolei w prawo lub lewo (Intelij)*
+* Refaktoryzacja polegała na dodaniu odpowiednich wcięć względem zaczętych klas i instrukcji */
